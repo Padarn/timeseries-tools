9aa4d1e877223ef0daab1b05d91d0e220cb35533
Padarn Wilson <padarn@gmail.com>
2014-05-13 10:54:12 +1000

    standardization functions

--- timeseriestools/tests/test_trend_removal.py
+++ timeseriestools/tests/test_trend_removal.py
@@ -21,56 +21,95 @@ from datetime import datetime
 
 class TestRemovalFilters(object):
 
-	def __init__(self):
-		np.random.seed(1234)
-		rng = pd.date_range('1/1/2011', periods=50*24, freq='1h')
-		self.ts = pd.Series(np.random.randn(len(rng)), index=rng)
-		self.linear = np.linspace(-10,10,len(self.ts))
-		self.linear = pd.Series(data=self.linear,index=self.ts.index)
-		self.trig = 10*np.sin(np.linspace(0,10*np.pi,len(self.ts)))
-		self.trig = pd.Series(data=self.trig,index=self.ts.index)
+    def __init__(self):
+        np.random.seed(1234)
+        rng = pd.date_range('1/1/2011', periods=50*24, freq='1h')
+        self.ts = pd.Series(np.random.randn(len(rng)), index=rng)
+        self.linear = np.linspace(-10,10,len(self.ts))
+        self.linear = pd.Series(data=self.linear,index=self.ts.index)
+        self.trig = 10*np.sin(np.linspace(0,10*np.pi,len(self.ts)))
+        self.trig = pd.Series(data=self.trig,index=self.ts.index)
 
-	def test_remove_linear_without_fit(self):
-		ts_plus_linear = self.ts + self.linear
-		ts = trend_removal.remove_linear_trend(ts_plus_linear, return_fit = False)
-		assert_array_almost_equal(ts,self.ts,1)
+    def test_remove_linear_without_fit(self):
+        ts_plus_linear = self.ts + self.linear
+        ts = trend_removal.remove_linear_trend(ts_plus_linear, return_fit = False)
+        assert_array_almost_equal(ts,self.ts,1)
 
-	def test_remove_linear_with_fit(self):
-		ts_plus_linear = self.ts + self.linear
-		ts, [linear_fit, fit] = trend_removal.remove_linear_trend(ts_plus_linear, return_fit = True)
-		assert_array_almost_equal(ts,self.ts,1)
-		assert_array_almost_equal(linear_fit,self.linear,1)
-		assert_array_almost_equal(np.array([20,-10]),fit,1)
+    def test_remove_linear_with_fit(self):
+        ts_plus_linear = self.ts + self.linear
+        ts, [linear_fit, fit] = trend_removal.remove_linear_trend(ts_plus_linear, return_fit = True)
+        assert_array_almost_equal(ts,self.ts,1)
+        assert_array_almost_equal(linear_fit,self.linear,1)
+        assert_array_almost_equal(np.array([20,-10]),fit,1)
 
-	def test_remove_trig_without_fit_order1(self):
-		ts_plus_trig = self.ts + self.trig
-		ts = trend_removal.remove_trig_trend(ts_plus_trig, period='10d' ,return_fit = False)
-		assert_array_almost_equal(ts,self.ts,1)
-
-
-	def test_remove_linear_without_fit_order2_overspecified(self):
-		ts_plus_trig = self.ts + self.trig
-		ts, [trig_fit, fit] = trend_removal.remove_trig_trend(ts_plus_trig, period='10d', order=2, return_fit = True)
-		##Note: This seems more sensitve than it should be.
-		assert_array_almost_equal(ts[10:20],self.ts[10:20],1) 
-
-	def test_remove_linear_with_fit_order1(self):
-		ts_plus_trig = self.ts + self.trig
-		ts, [trig_fit, fit] = trend_removal.remove_trig_trend(ts_plus_trig, period='10d' ,return_fit = True)
-		assert_array_almost_equal(ts,self.ts,1)
-		assert_array_almost_equal(trig_fit,self.trig,1)
-		assert_array_almost_equal(np.array([0.0, 10.0, 0]),fit,1)
-
-	def test_remove_trend_linear_no_coef(self):
-		ts_plus_linear = self.ts + self.linear
-		ts = trend_removal.remove_ts_trend(ts_plus_linear, self.linear, return_coef = False)
-		assert_array_almost_equal(ts,self.ts,1)
-
-	def test_remove_trend_linear_coef(self):
-		ts_plus_linear = self.ts+ self.linear
-		ts, coef = trend_removal.remove_ts_trend(ts_plus_linear, self.linear, return_coef = True)
-		assert_array_almost_equal(ts,self.ts,1)
-		assert_almost_equal(coef,1,1)
+    def test_remove_trig_without_fit_order1(self):
+        ts_plus_trig = self.ts + self.trig
+        ts = trend_removal.remove_trig_trend(ts_plus_trig, period='10d' ,return_fit = False)
+        assert_array_almost_equal(ts,self.ts,1)
 
 
+    def test_remove_linear_without_fit_order2_overspecified(self):
+        ts_plus_trig = self.ts + self.trig
+        ts, [trig_fit, fit] = trend_removal.remove_trig_trend(ts_plus_trig, period='10d', order=2, return_fit = True)
+        ##Note: This seems more sensitve than it should be.
+        assert_array_almost_equal(ts[10:20],self.ts[10:20],1) 
+
+    def test_remove_linear_with_fit_order1(self):
+        ts_plus_trig = self.ts + self.trig
+        ts, [trig_fit, fit] = trend_removal.remove_trig_trend(ts_plus_trig, period='10d' ,return_fit = True)
+        assert_array_almost_equal(ts,self.ts,1)
+        assert_array_almost_equal(trig_fit,self.trig,1)
+        assert_array_almost_equal(np.array([0.0, 10.0, 0]),fit,1)
+
+    def test_remove_trend_linear_no_coef(self):
+        ts_plus_linear = self.ts + self.linear
+        ts = trend_removal.remove_ts_trend(ts_plus_linear, self.linear, return_coef = False)
+        assert_array_almost_equal(ts,self.ts,1)
+
+    def test_remove_trend_linear_coef(self):
+        ts_plus_linear = self.ts+ self.linear
+        ts, coef = trend_removal.remove_ts_trend(ts_plus_linear, self.linear, return_coef = True)
+        assert_array_almost_equal(ts,self.ts,1)
+        assert_almost_equal(coef,1,1)
+
+
+class TestStandardizationScaling(object):
+
+    def __init__(self):
+        np.random.seed(1234)
+        rng = pd.date_range('1/1/2011', periods=24*50, freq='1h')
+        self.mu = 5
+        self.mu2 = -3
+        self.sigma = 2
+        self.sigma2 = 3
+        self.ts = pd.Series(np.random.randn(len(rng))*self.sigma+self.mu, index=rng)
+        self.df = pd.DataFrame(index=self.ts.index)
+        self.df['ts1']=pd.Series(np.random.randn(len(rng))*self.sigma+self.mu, index=rng)
+        self.df['ts2']=pd.Series(np.random.randn(len(rng))*self.sigma2+self.mu2, index=rng)
+
+    def test_ts_set_hour_mean(self):
+        ts = self.ts
+        ts = trend_removal.change_mean(ts)
+        assert_almost_equal(ts.mean(), 0)
+        ts = trend_removal.change_mean(ts, 15)
+        assert_almost_equal(ts.mean(),15)
+
+    def test_df_set_sd(self):
+        df = self.df
+        df = trend_removal.change_sd(df)
+        assert_almost_equal(df['ts1'].std(), 1)
+        assert_almost_equal(df['ts2'].std(), 1)
+        df = trend_removal.change_sd(df, {'ts1':15,'ts2':5})
+        assert_almost_equal(df['ts1'].std(), 15)
+        assert_almost_equal(df['ts2'].std(), 5)
+
+    def test_ts_standardize(self):
+        trend_removal.standardize(self.ts)
+        ts = self.ts
+        ts, old = trend_removal.standardize(ts, return_old=True)
+        assert_almost_equal(ts.mean(), 0)
+        assert_almost_equal(ts.std(),1)
+        ts = trend_removal.standardize(ts, old_values=old)
+        assert_almost_equal(ts.mean(),self.ts.mean())
+        assert_almost_equal(ts.std(),self.ts.std())
 
--- timeseriestools/trend_removal.py
+++ timeseriestools/trend_removal.py
@@ -4,6 +4,16 @@ import time
 import pandas as pd
 import statsmodels.api as sm
 
+def _apply_to_each_col(df, func, param_list):
+    """ 
+    Generic function to apply a function to each col
+    in a df that has key in param_list
+    """
+    df = df.copy()
+    for key in param_list.keys():
+        df[key] = func(df[key],param_list[key])
+    return df
+
 def _toYearFraction(date):
     """ 
     Converts a date time to a float representation.
@@ -29,9 +39,15 @@ def remove_ts_trend(ts, trend_ts, return_coef = False):
 
     Parameters
     ----------
-    ts - time series for which to remove the linear trend.
-    trend_ts - trend time series to remove a multiple of.
+    ts          - time series for which to remove the linear trend.
+    trend_ts    - trend time series to remove a multiple of.
     return_coef - if true, also returns the multiplicative coef.
+
+    Output
+    -----
+    returns the trend removed series, and if return_coef is true
+    returns as a second argument the coefficient
+    of the fit.
     """
     mod = sm.OLS(ts.values, trend_ts.values)
     res = mod.fit()
@@ -47,11 +63,17 @@ def remove_linear_trend(ts, return_fit = False):
 
     Parameters
     ----------
-    ts - time series for which to remove the linear trend.
+    ts         - time series for which to remove the linear trend.
     return_fit - bool, decides if fit is returned as second argument.
                  If true, returns the trend and coefficients:
                        new_ts, [linear_fit, fit]
                  fit is an array fit = [b1,b0], y=b0+b1*x
+
+    Output
+    -----
+    returns the trend removed series, and if return_coef is true
+    returns as a second argument the fitted series and coefficient
+    of the fit.
     """
     
     # Scale times
@@ -75,13 +97,19 @@ def remove_trig_trend(ts, period, order=1, return_fit = False):
 
     Parameters
     ----------
-    ts - time series for which to remove the trig trend.
-    period - the period of the longest trig cycle.
-    order - the order of the trig fit.
+    ts         - time series for which to remove the trig trend.
+    period     - the period of the longest trig cycle.
+    order      - the order of the trig fit.
     return_fit - bool, decides if fit is returned as second argument.
                  If true, returns the trend and coefficients:
                        new_ts, [linear_fit, fit]
                  fit is an array fit = [b1,b0], y=b0+b1*x
+
+    Output
+    -----
+    returns the trend removed series, and if return_coef is true
+    returns as a second argument the fitted series and coefficient
+    of the fit.
     """
 
     times = np.array([_toYearFraction(x) for x in ts.index])
@@ -104,4 +132,134 @@ def remove_trig_trend(ts, period, order=1, return_fit = False):
         return ts - trig_fit, [trig_fit, res.params]
     else:
         return ts - trig_fit
+
+
+def _change_mean(ts, new_mean):
+    """
+    Performs change_mean on one time series. See doc string of change_mean
+    """
+
+    return ts-ts.mean()+new_mean
+
+def change_mean(tsframe, new_mean = None):
+    """
+    Changes the mean of a time series, or of all columns of a dataframe. 
+    If no new mean is specified, changes the mean to zero.
+
+    Parameters
+    ----------
+    ts_frame - dataframe or time series to change mean of
+    new_mean - new mean for data, if dataframe should be dict
+
+    Output
+    ------
+    the new series/dataframe with changed mean
+    """
+
+    if isinstance(tsframe, pd.DataFrame):
+        if new_mean is None:
+            new_mean = {}
+            for key in tsframe.keys():
+                new_mean[key]=0
+        return _apply_to_each_col(tsframe, _change_mean, new_mean)
+    else:
+        if new_mean is None:
+            return _change_mean(tsframe, 0)
+        else:
+            return _change_mean(tsframe, new_mean)
+
+
+def _change_sd(ts, new_sd):
+    """
+    Performs change_sd on one time series. See doc string of change_sd
+    """
+
+    return ts/ts.std()*new_sd
+
+def change_sd(tsframe, new_sd = None):
+    """
+    Changes the standard deviation of a time series, or of all columns of 
+    a dataframe. If no new standard deviation is specified, changes the 
+    standard deviation to zero.
+
+    Parameters
+    ----------
+    ts_frame - dataframe or time series to change standard deviation of
+    new_sd   - new standard deviation for data, if dataframe should be dict
+
+    Output
+    ------
+    the new series/dataframe with changed standard deviation.
+    """
+
+    if isinstance(tsframe, pd.DataFrame):
+        if new_sd is None:
+            new_sd = {}
+            for key in tsframe.keys():
+                new_sd[key]=1
+        return _apply_to_each_col(tsframe, _change_sd, new_sd)
+    else:
+        if new_sd is None:
+            return _change_sd(tsframe, 1)
+        else:
+            return _change_sd(tsframe, new_sd)
+
+
+def _standardize(ts, old_values = None, return_old = False):
+    """
+    Performs standardize on one time series. See doc string of standardize
+    """
+
+    if old_values == None:
+        mean = ts.mean()
+        sd = ts.std()
+        if return_old:
+            return (ts-mean)/sd, [mean, sd]
+        else:
+            return (ts-mean)/sd
+    else:
+        old_mean = old_values[0]
+        old_sd = old_values[1]
+        return ts*old_sd+old_mean
+
+def standardize(tsframe, old_values = None, return_old = False):
+    """
+    Standardizes a time series or all columns of a dataframe. That is the mean
+    is set to zero and the standard deviation to ones. If old mean and standard
+    deviation are provided then the process is reversed instead.
+
+    Parameters
+    ----------
+    tsframe -    dataframe or time seires to standardize
+    old_values - old values of mean and standard deiviation in a dict if is to be
+                 reversed.
+    return_old - if true, old mean and standard deviations are returned.
+
+    Output
+    ------
+    the new standardized (or reversed) series/dataframe, and also the old mean 
+    and standard deviation as a second return argument is return_old=True
+    """
+
+    # NOTE : THIS SHOULD BE GENERALISED SO DON"T NEED ALL THIS 
+    # EXTRA STUFF THAT SHOULD BE HANDLED BY  _apply_to_each_col 
+    if isinstance(tsframe, pd.DataFrame):
+        if old_values is None:
+            old_values = {}
+            df = tsframe.copy()
+            for key in param_list.keys():
+                df[key], old_values[key] =  _standardize(df[key], 
+                                              return_old = True)
+            if return_old:
+                return df, old_values
+            else:
+                return df
+    else:
+        if old_values is None:
+            if return_old:
+                return _standardize(tsframe, return_old = True)
+            else:
+                return _standardize(tsframe)
+        else:
+            return _standardize(tsframe, old_values = old_values)
 
